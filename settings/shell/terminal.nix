16708d449166f7b2f1448fc4870ca90e124928bd
sfuentes <sfuentes@mail.com>
2024-05-22 00:27:47 -0300

    Change Kitty for Wezterm

--- settings/shell/terminal.nix
+++ settings/shell/terminal.nix
@@ -5,42 +5,31 @@
 }: {
   programs.wezterm = {
     enable = true;
-  };
 
-  programs.foot = {
-    enable = true;
-  };
+    enableZshIntegration = true;
 
-  programs.kitty = {
-    enable = true;
+    extraConfig = ''
+      local wezterm = require 'wezterm'
+      local config = {}
+      config.default_prog = { 'zsh' }
+      config.font = wezterm.font 'monospace'
+      config.window_background_opacity = 0.5
 
-    settings = {
-      enable_audio_bell = false;
-      background_opacity = "0.5";
-      shell = "${pkgs.zsh}/bin/zsh"; # Default terminal
-      window_padding_width = 10;
-      window_margin_width = "0 0 50";
-      font_size = 14;
-    };
-
-    keybindings = {
-      "ctrl+c" = "copy_or_interrupt"; # Copy when text is selected
-    };
-
-    shellIntegration.enableZshIntegration = true;
+      return config
+    '';
   };
 
   wayland.windowManager.hyprland.settings.exec-once =
     [
-      "[workspace 11 silent] kitty"
+      "[workspace 11 silent] wezterm"
     ]
     ++ (
       if user.machine == "desktop"
       then [
-        "[workspace 3 silent] kitty"
+        "[workspace 3 silent] wezterm"
       ]
       else []
     );
 
-  programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite."class<kitty>" = "󰆍";
+  programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite."class<org.wezfurlong.wezterm>" = "󰆍";
 }
