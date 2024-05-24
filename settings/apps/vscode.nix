42656d3a5e24fff127ca9a486bb8014e7c70159b
sfuentes <sfuentes@mail.com>
2024-05-24 01:23:48 -0300

    changing settings

--- settings/apps/vscode.nix
+++ settings/apps/vscode.nix
@@ -2,9 +2,10 @@
   programs.vscode = {
     enable = true;
     enableUpdateCheck = false; # Prevent from showing message of update is available.
+
     userSettings = {
       "editor.defaultFormatter" = "esbenp.prettier-vscode"; # Use prettier to format the code
-      "editor.formatOnSave" = true; # Format on save
+      "editor.formatOnSave" = true;
       "editor.detectIndentation" = false;
 
       # Font configuration
@@ -14,12 +15,26 @@
       # Change / Update the imports when file is moved / renamed
       "javascript.updateImportsOnFileMove.enabled" = "always";
       "typescript.updateImportsOnFileMove.enabled" = "always";
-      
+
       "merge-conflict.autoNavigateNextConflict.enabled" = true; # During a pull request, auto navigate to next conflict
       "npm.autoDetect" = "off"; # Turn off autodetect npm
       "workbench.iconTheme" = "file-icons"; # Icons
+      "workbench.startupEditor" = "none";
+      "window.menuBarVisibility" = "hidden";
+      "window.enableMenuBarMnemonics" = false;
+      "window.customMenuBarAltFocus" = false;
     };
+
+    /*
+       keybindings = [{
+      key = "ctrl";
+      command = "workbench.action.toggleMenuBar";
+      #when = "";
+    }];
+    */
   };
 
   programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite."class<Code>" = "󰨞";
+
+  home.packages = [pkgs.alejandra];
 }
