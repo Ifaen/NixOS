d818138a1614cf71714f5969592d949d78552c53
sfuentes <sfuentes@mail.com>
2024-05-24 01:23:28 -0300

    temporary comment

--- settings/apps/thunderbird.nix
+++ settings/apps/thunderbird.nix
@@ -5,7 +5,7 @@
       isDefault = true;
     };
   };
-  wayland.windowManager.hyprland.settings.exec-once = ["[workspace 14 silent] thunderbird"];
+  #wayland.windowManager.hyprland.settings.exec-once = ["[workspace 14 silent] thunderbird"];
 
   programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite."class<thunderbird>" = "󱗆"; # nf-md-bird
 }
