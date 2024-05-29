1eb0cb3ea15462fbe1a6e6f0aea9daa110a95702
sfuentes <sfuentes@mail.com>
2024-05-29 02:11:41 -0300

    Deleted: xfconf and thunar. Disabled: partition-manager

--- settings/services/file-management.nix
+++ settings/services/file-management.nix
@@ -6,16 +6,7 @@
 }: {
   # File Manager
   programs = {
-    thunar = {
-      enable = false;
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-volman
-        thunar-media-tags-plugin
-      ];
-    };
-    partition-manager.enable = true;
-    xfconf.enable = true;
+    partition-manager.enable = false;
   };
 
   users.users.${user.name}.extraGroups = ["storage"]; # For disk management in file managers
@@ -57,35 +48,12 @@
           "text/html" = "librewolf.desktop";
           "x-scheme-handler/http" = "librewolf.desktop";
           "x-scheme-handler/https" = "librewolf.desktop";
-          "image/webp" = "librewolf.desktop";
-
-          "inode/directory" = "lf.desktop vlc.desktop";
 
           "image/gif" = "vlc.desktop";
-          "video/3gpp" = "vlc.desktop";
-          "video/mp2t" = "vlc.desktop";
-          "video/mp4" = "vlc.desktop";
-          "video/mpeg" = "vlc.desktop";
-          "video/webm" = "vlc.desktop";
 
           "application/pdf" = "org.pwmt.zathura.desktop";
 
           "x-scheme-handler/tg" = "userapp-Telegram Desktop-4V96K2.desktop";
-
-          #"application/vnd.microsoft.portable-executable" = "wine.desktop"; # Open .exe windows apps with wine
-
-          #"application/x-7z-compressed" = ; TODO for .7z files
-          #"audio/3gpp2" = ; TODO for .3g2 files
-          #"application/msword" = ""; # TODO for .doc files
-          #"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = ""; # TODO for .docx files
-          #"image/vnd.microsoft.icon" = ""; TODO For .ico files
-          #"audio/webm" = ""; TODO for .weba files
-          #"application/vnd.ms-powerpoint" = ""; # TODO for .ppt files
-          #"audio/mpeg" = ""; find proper for .mp3 files
-          #"application/vnd.openxmlformats-officedocument.presentationml.presentation" = ""; TODO for .pptx files
-          #"application/vnd.rar" = ""; TODO find for .rar files
-          #"application/x-tar" = ""; TODO for .tar files
-          #"application/zip" = ""; TODO for .zip  files
         };
       };
 
@@ -107,9 +75,9 @@
     };
 
     programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite = {
-      "class<thunar>" = "";
       "class<libreoffice-calc>" = "󱎏";
       "class<zathura>" = "";
+      "class<vlc>" = "󰕼"; # nf-md-vlc
       "class<org.kde.partitionmanager>" = "󰋊"; # nf-md-harddisk
     };
   };
