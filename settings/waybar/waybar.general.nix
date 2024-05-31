d59c027716202f29ca18aaca67fd0c3608dbe39c
sfuentes <sfuentes@mail.com>
2024-05-31 18:25:35 -0300

    Moved: Waybar css to waybar folder

--- settings/waybar/waybar.general.nix
+++ settings/waybar/waybar.general.nix
@@ -5,12 +5,14 @@
   ...
 }: {
   home-manager.users.${user.name} = {
-    programs.waybar.enable = true;
+    programs.waybar = {
+      enable = true;
 
-    style = ''
-      @import "${user.home}/.cache/wal/colors-waybar.css";        /* For the color scheme */
-      @import "${user.flake}/settings/waybar/waybar.style.css";  /* For the styling*/
-    '';
+      style = ''
+        @import "${user.home}/.cache/wal/colors-waybar.css";        /* For the color scheme */
+        @import "${user.flake}/settings/waybar/waybar.style.css";  /* For the styling*/
+      '';
+    };
 
     # Settings of waybar in other modules
     wayland.windowManager.hyprland.settings.exec-once = ["waybar"];
