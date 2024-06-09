c70cfb2fe00ca8974967c479b38f2be1e0ff3faa
sfuentes <sfuentes@mail.com>
2024-06-09 18:35:38 -0300

    Added: Preferences attributes and bookmarks

--- settings/firefox/firefox.profile.default.nix
+++ settings/firefox/firefox.profile.default.nix
@@ -18,10 +18,52 @@
       }
     '';
 
+    settings = {
+      "browser.startup.page" = 3; # 3 to resume previous tabs. Default: 1
+      "trailhead.firstrun.didSeeAboutWelcome" = true;
+    };
+
     bookmarks = [
+      # Nix
+      {
+        name = "Nix";
+        bookmarks = [
+          {
+            name = "Nix Packages";
+            url = "https://search.nixos.org/packages?channel=unstable";
+            keyword = "np";
+          }
+          {
+            name = "MyNixOS";
+            url = "https://mynixos.com/";
+            keyword = "myn";
+          }
+        ];
+      }
+      # Entertainment
+      {
+        name = "Entertainment";
+        bookmarks = [
+          {
+            name = "Youtube";
+            url = "https://www.youtube.com";
+            keyword = "yt";
+          }
+          {
+            name = "Manganato";
+            url = "https://manganato.com";
+            keyword = "manga";
+          }
+          {
+            name = "VisorTMO";
+            url = "https://visortmo.com";
+            keyword = "visor";
+          }
+        ];
+      }
+      # Social Media
       {
         name = "Social Media";
-        toolbar = true;
         bookmarks = [
           {
             name = "Whatsapp";
@@ -55,24 +97,77 @@
           }
         ];
       }
+      # E-commerce
       {
-        name = "Tools";
+        name = "E-commerce";
         toolbar = true;
         bookmarks = [
           {
+            name = "Solo Todo";
+            url = "https://www.solotodo.cl/";
+            keyword = "solotodo";
+          }
+        ];
+      }
+      # Tools
+      {
+        name = "Tools";
+        bookmarks = [
+          {
+            name = "Firefox Profiles";
+            url = "about:profiles";
+            keyword = "prof";
+          }
+          {
             name = "Canva";
-            url = "https://www.canva.com/";
+            url = "https://www.canva.com";
             keyword = "canva";
           }
           {
             name = "GitLab";
-            url = "https://gitlab.com/aventuras-en-la-patagonia/Webpage";
+            url = "https://gitlab.com";
             keyword = "glab";
+          }
+          {
+            name = "Github";
+            url = "https://github.com";
+            keyword = "ghub";
           }
           {
             name = "Tabler.io";
             url = "tabler.io/icons";
             keyword = "icons";
+          }
+          {
+            name = "ChatGPT";
+            url = "https://chatgpt.com";
+            keyword = "gpt";
+          }
+          {
+            name = "Atlassian";
+            url = "https://www.atlassian.com";
+            keyword = "atl";
+          }
+          {
+            name = "Firebase";
+            url = "https://firebase.google.com";
+            keyword = "fbase";
+          }
+        ];
+      }
+      # University
+      {
+        name = "University";
+        bookmarks = [
+          {
+            name = "Intranet";
+            url = "https://portal.unab.cl";
+            keyword = "intra";
+          }
+          {
+            name = "CAE";
+            url = "http://ingresa.cl";
+            keyword = "cae";
           }
         ];
       }
