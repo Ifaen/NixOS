1e9240109dfe5191ba45be3ef4cc7f3622d22e55
sfuentes <sfuentes@mail.com>
2024-06-09 18:33:56 -0300

    Added: Obsidian icon
    Also, formatted pkgs.stable.gns3-server

--- settings/packages.nix
+++ settings/packages.nix
@@ -16,9 +16,7 @@
       dynamips # Emulator computer program that was written to emulate Cisco routers
       gns3-gui # Graphical user interface for controlling the GNS3
       ubridge # Bridge for UDP tunnels, Ethernet, TAP, and VMnet interfaces
-    ])
-    ++ (with pkgs.stable; [
-      gns3-server # Graphical Network Simulator 3 server
+      stable.gns3-server # Graphical Network Simulator 3 server
     ])
     # Packages specific to device
     ++ (
@@ -31,14 +29,11 @@
     );
 
   ## USER PACKAGES
-  home-manager.users.${user.name} = {
-    home.packages = [];
-
-    programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite = {
-      "class<com.rtosta.zapzap>" = "󰖣";
-      "class<gns3>" = "󱂇";
-      "class<org.telegram.desktop>" = "";
-      "class<zoom>" = "";
-    };
+  home-manager.users.${user.name}.programs.waybar.settings.statusBar."hyprland/workspaces".window-rewrite = {
+    "class<obsidian>" = "󰎚"; # nf-md-note
+    "class<com.rtosta.zapzap>" = "󰖣";
+    "class<gns3>" = "󱂇";
+    "class<org.telegram.desktop>" = "";
+    "class<zoom>" = "";
   };
 }
