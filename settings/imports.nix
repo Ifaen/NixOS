a74499e7596a55d6dbc0a61270a1339b3568e9a0
sfuentes <sfuentes@mail.com>
2024-06-09 18:33:06 -0300

    Removed: eww folder and from imports

--- settings/imports.nix
+++ settings/imports.nix
@@ -1,46 +1,66 @@
 {user, ...}: {
   imports =
     [
+      ./ags/ags.config.nix # Widgets
+
       ./discord/discord.config.nix # Voice chat
+
       ./dunst/dunst.config.nix # Notification daemon
-      ./eww/eww.config.nix # Status bar
+
       ./firefox/firefox.config.nix # Web browser
       ./firefox/firefox.profile.aventuraspatagonia.nix
       ./firefox/firefox.profile.default.nix
+
       ./git/git.config.nix # Code version control
+
       ./hardware/hardware.config.nix # INITIALLY REPLACE CONTENT WITH /etc/nixos/hardware-configuration.nix
       ./hardware/hardware.drivers.nix # Few drivers depending of the hardware
+
       ./hypr/hypridle.nix # Idle management daemon
       ./hypr/hyprland.config.nix # Window manager
       ./hypr/hyprland.keybinds.nix # Window manager keybindings
       ./hypr/hyprlock.nix # System password lock
+
       ./lf/lf.config.nix # Terminal file browser configuration
       ./lf/lf.keybinds.nix # Terminal file browser keybinds
       ./lf/lf.previewer.nix # Terminal file browser previewer
+
       ./sddm/sddm.config.nix # Display manager
+
       ./secrets/secrets.keepassxc.nix
       ./secrets/secrets.networking.nix
       ./secrets/secrets.polkit.nix
       ./services/services.sound.nix
+
       ./starship/starship.config.nix # Shell prompt customization
+
       ./themes/themes.config.nix
       ./themes/themes.fonts.nix
+
       ./thunderbird/thunderbird.config.nix # Email manager
+
       ./vscode/vscode.config.nix
       ./vscode/vscode.keybinds.nix
       ./vscode/vscode.settings.nix
+
       ./waybar/waybar.config.nix # Status bar
       ./waybar/waybar.settings.nix # Status bar settings
+
       ./wezterm/wezterm.config.nix # Terminal simulator
       ./wezterm/wezterm.keybinds.nix # Terminal simulator keybindings
+
       ./wlogout/wlogout.config.nix # Logout interface
+
       ./wofi/wofi.config.nix # Software selector
+
       ./xdg/xdg.config.nix
       ./xdg/xdg.directories.nix
       ./xdg/xdg.mimeapps.nix
       ./xdg/xdg.portal.nix
+
       ./xremap/xremap.config.nix # Dynamic keybinds service
       ./xremap/xremap.keybinds.nix # Dynamic keybinds service keybindings
+
       ./zsh/zsh.config.nix # Terminal shell configuration
       ./zsh/zsh.tools.nix # Terminal shell tools..
     ]
