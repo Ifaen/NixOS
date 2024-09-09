7ebb8e0c5c8acdc5eec02980f0e856a92cbac0b9
sfuentes <sfuentes@mail.com>
2024-09-09 15:50:29 -0300

    Removed notebook if conditionals and devshells

--- flake.nix
+++ flake.nix
@@ -5,9 +5,9 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixpkgs.url = "nixpkgs/nixos-24.05";
     home-manager = {
-        url = "github:nix-community/home-manager/release-24.05";
-        inputs.nixpkgs.follows = "nixpkgs";
-    };    
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur.url = "github:nix-community/nur";
     ags.url = "github:Aylur/ags";
     xremap-flake.url = "github:xremap/nix-flake";
@@ -22,33 +22,19 @@
     ags,
     xremap-flake,
   }: let
-    machine = builtins.readFile ./.others/machine; # Read from file the machine name, desktop or notebook
-
-    user =
-      {
-        fullname = "Santiago Fuentes";
-        name = "sfuentes";
-        home = "/home/${user.name}";
-        flake = "${user.home}/NixOS";
-        mail = "sfuentes@mail.com";
-        language = "us";
-        system = "x86_64-linux";
-        inherit machine;
-      }
-      // (
-        if (machine == "desktop")
-        then {
-          monitor = "DP-1";
-          monitor2 = "HDMI-A-1";
-        }
-        else {
-          monitor = "eDP-1";
-        }
-      );
-
-    pkgs = nixpkgs.legacyPackages.${user.system};
+    user = {
+      fullname = "Santiago Fuentes";
+      name = "sfuentes";
+      home = "/home/${user.name}";
+      flake = "${user.home}/NixOS";
+      mail = "sfuentes@mail.com";
+      language = "us";
+      system = "x86_64-linux";
+      machine = "desktop";
+      monitor = "HDMI-A-1";
+    };
   in {
-    nixosConfigurations.${machine} = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.${user.machine} = nixpkgs.lib.nixosSystem {
       system = user.system;
 
       specialArgs = {
@@ -66,13 +52,6 @@
         /config.nix
         /imports.nix
         /packages.nix
-      ];
-    };
-
-    devShells.${user.system}.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        nurl # Shows information about a package in a repository
-        file # Shows information of files
       ];
     };
   };
