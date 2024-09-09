8b892ef8e52677229ba0b67556c17097a161392b
sfuentes <sfuentes@mail.com>
2024-09-09 15:56:16 -0300

    Removed monitor conditionals

--- settings/hypr/hyprland.config.nix
+++ settings/hypr/hyprland.config.nix
@@ -14,6 +14,7 @@
 
       animations = {
         enabled = "yes";
+        /*
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
         animation = [
           "windows, 1, 7, myBezier"
@@ -23,6 +24,7 @@
           "fade, 1, 7, default"
           "workspaces, 1, 6, default"
         ];
+        */
       };
 
       decoration = {
@@ -55,58 +57,12 @@
 
       gestures.workspace_swipe = "off";
 
-      input =
-        {
-          follow_mouse = 1;
-        }
-        // (
-          if user.machine == "desktop"
-          then {
-            sensitivity = 1; # range [-1.0, 1.0], 0 means no modification.
-          }
-          else {
-            sensitivity = 0;
-
-            touchpad = {
-              natural_scroll = "no";
-              disable_while_typing = false;
-            };
-          }
-        );
-
-      #master.new_is_master = false;
+      input = {
+        follow_mouse = 1;
+        sensitivity = 1; # range [-1.0, 1.0], 0 means no modification.
+      };
 
       misc.disable_hyprland_logo = true;
-
-      monitor =
-        if user.machine == "notebook"
-        then "${user.monitor},1920x1200, auto, 1"
-        else [
-          "${user.monitor}, highres, 1920x0, 1"
-          "${user.monitor2}, highres, 0x0, 1"
-        ];
-
-      workspace =
-        if user.machine == "desktop" # When second monitor is turn off, workspaces move to default monitor
-        then [
-          "1, monitor:${user.monitor}, default:true"
-          "2, monitor:${user.monitor}"
-          "3, monitor:${user.monitor}"
-          "4, monitor:${user.monitor}"
-          "5, monitor:${user.monitor}"
-          # Workspaces 5 to 10 are to move workspaces 1 to 5 between monitors
-          "6, monitor:${user.monitor2}"
-          "7, monitor:${user.monitor2}"
-          "8, monitor:${user.monitor2}"
-          "9, monitor:${user.monitor2}"
-          "10, monitor:${user.monitor2}"
-          # Workspaces with specific softwares
-          "11, monitor:${user.monitor2}, default:true"
-          "12, monitor:${user.monitor2}"
-          "13, monitor:${user.monitor2}, on-created-empty:keepassxc"
-          "14, monitor:${user.monitor2}"
-        ]
-        else [];
     };
   };
 }
