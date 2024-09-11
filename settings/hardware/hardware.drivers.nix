d8d8ff2bb294b1937b8d1e885c9eaf6a02bfed07
sfuentes <sfuentes@mail.com>
2024-09-11 15:55:00 -0300

    Removed conditional of user.machine

--- settings/hardware/hardware.drivers.nix
+++ settings/hardware/hardware.drivers.nix
@@ -4,32 +4,26 @@
   user,
   ...
 }: {
-  config =
-    {
-      zramSwap.enable = true; # Enable the usage of zram instead of swap memory
+  config = {
+    zramSwap.enable = true; # Enable the usage of zram instead of swap memory
 
-      # Using cpufreq-info | grep "governor" in nix-shell -p cpufrequtils, outputs available options. When doing cat
-      # /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor, the output was powersave which made the system slower in theory.
-      powerManagement.cpuFreqGovernor = "performance";
-    }
-    // (
-      if user.machine == "desktop"
-      then {
-        boot.supportedFilesystems = ["ntfs"]; # Allow the support for windows file system
+    # Using cpufreq-info | grep "governor" in nix-shell -p cpufrequtils, outputs available options. When doing cat
+    # /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor, the output was powersave which made the system slower in theory.
+    powerManagement.cpuFreqGovernor = "performance";
 
-        # Drivers
-        boot = {
-          #kernelPackages = ;
-          kernelModules = ["amdgpu"]; # To boot kernel with amd module
-        };
+    boot.supportedFilesystems = ["ntfs"]; # Allow the support for windows file system
 
-        hardware.opengl = {
-          enable = true; # Mesa
+    # Drivers
+    boot = {
+      #kernelPackages = ;
+      kernelModules = ["amdgpu"]; # To boot kernel with amd module
+    };
 
-          #driSupport = true; # radv: an open-source Vulkan driver from freedesktop
-          driSupport32Bit = true; # radv support for 32 bits applications
-        };
-      }
-      else {}
-    );
+    hardware.opengl = {
+      enable = true; # Mesa
+
+      #driSupport = true; # radv: an open-source Vulkan driver from freedesktop
+      driSupport32Bit = true; # radv support for 32 bits applications
+    };
+  };
 }
--- settings/xremap/xremap.keybinds.nix
+++ settings/xremap/xremap.keybinds.nix
@@ -90,155 +90,148 @@
     fi
   ''}";
 in {
-  home-manager.users.${user.name}.services.xremap.config.keymap =
-    [
-      # Applications
-      {
-        name = "applications";
-        remap = {
-          control-q.launch = [
-            "hyprctl"
-            "dispatch"
-            "killactive"
-          ];
+  home-manager.users.${user.name}.services.xremap.config.keymap = [
+    # Applications
+    {
+      name = "applications";
+      remap = {
+        control-q.launch = [
+          "hyprctl"
+          "dispatch"
+          "killactive"
+        ];
 
-          super-w.launch = [
-            "hyprctl"
-            "dispatch"
-            "exec"
-            "${pkgs.writeShellScript "toggle-wofi" ''pkill wofi || wofi --normal-window --allow-images --show drun''}"
-          ];
+        super-w.launch = [
+          "hyprctl"
+          "dispatch"
+          "exec"
+          "${pkgs.writeShellScript "toggle-wofi" "pkill wofi || wofi --normal-window --allow-images --show drun"}"
+        ];
 
-          super-shift-w.launch = [
-            "hyprctl"
-            "dispatch"
-            "exec"
-            "${pkgs.writeShellScript "toggle-waybar" "pkill waybar || waybar"}"
-          ];
+        super-shift-w.launch = [
+          "hyprctl"
+          "dispatch"
+          "exec"
+          "${pkgs.writeShellScript "toggle-waybar" "pkill waybar || waybar"}"
+        ];
 
-          # Screenshot utility
-          sysrq.launch = [
-            "${pkgs.hyprshot}/bin/hyprshot"
-            "--mode"
-            "region"
-            "--freeze"
-            "--clipboard-only"
-          ];
-          shift-sysrq.launch = [
-            "${pkgs.hyprshot}/bin/hyprshot"
-            "--mode"
-            "region"
-            "--freeze"
-            "--output-folder"
-            "${user.home}/Media/Screenshots"
-          ];
-        };
-      }
-      # Workspace
-      {
-        name = "workspace";
-        remap = {
-          # Change wallpaper with super + up and down buttons
-          super-up.launch = ["${wallpaper-change}" "up"];
-          super-down.launch = ["${wallpaper-change}" "down"];
+        # Screenshot utility
+        sysrq.launch = [
+          "${pkgs.hyprshot}/bin/hyprshot"
+          "--mode"
+          "region"
+          "--freeze"
+          "--clipboard-only"
+        ];
+        shift-sysrq.launch = [
+          "${pkgs.hyprshot}/bin/hyprshot"
+          "--mode"
+          "region"
+          "--freeze"
+          "--output-folder"
+          "${user.home}/Media/Screenshots"
+        ];
+      };
+    }
+    # Workspace
+    {
+      name = "workspace";
+      remap = {
+        # Change wallpaper with super + up and down buttons
+        super-up.launch = ["${wallpaper-change}" "up"];
+        super-down.launch = ["${wallpaper-change}" "down"];
 
-          # Change workspace with super + left and right buttons
-          super-left.launch = ["hyprctl" "dispatch" "workspace" "e-1"];
-          super-right.launch = ["hyprctl" "dispatch" "workspace" "e+1"];
+        # Change workspace with super + left and right buttons
+        super-left.launch = ["hyprctl" "dispatch" "workspace" "e-1"];
+        super-right.launch = ["hyprctl" "dispatch" "workspace" "e+1"];
 
-          # Change window with super + shift + arrow keys
-          super-shift-up.launch = ["hyprctl" "dispatch" "movefocus" "u"];
-          super-shift-down.launch = ["hyprctl" "dispatch" "movefocus" "d"];
-          super-shift-left.launch = ["hyprctl" "dispatch" "movefocus" "l"];
-          super-shift-right.launch = ["hyprctl" "dispatch" "movefocus" "r"];
+        # Change window with super + shift + arrow keys
+        super-shift-up.launch = ["hyprctl" "dispatch" "movefocus" "u"];
+        super-shift-down.launch = ["hyprctl" "dispatch" "movefocus" "d"];
+        super-shift-left.launch = ["hyprctl" "dispatch" "movefocus" "l"];
+        super-shift-right.launch = ["hyprctl" "dispatch" "movefocus" "r"];
 
-          # Change size of window with super + ctrl + arrow keys
-          super-control-up.launch = ["hyprctl" "dispatch" "resizeactive" "0" "-10"];
-          super-control-down.launch = ["hyprctl" "dispatch" "resizeactive" "0" "10"];
-          super-control-left.launch = ["hyprctl" "dispatch" "resizeactive" "-10" "0"];
-          super-control-right.launch = ["hyprctl" "dispatch" "resizeactive" "10" "0"];
+        # Change size of window with super + ctrl + arrow keys
+        super-control-up.launch = ["hyprctl" "dispatch" "resizeactive" "0" "-10"];
+        super-control-down.launch = ["hyprctl" "dispatch" "resizeactive" "0" "10"];
+        super-control-left.launch = ["hyprctl" "dispatch" "resizeactive" "-10" "0"];
+        super-control-right.launch = ["hyprctl" "dispatch" "resizeactive" "10" "0"];
 
-          # Move window in the same workspace with super + alt + arrow keys
-          super-alt-up.launch = ["hyprctl" "dispatch" "movewindow" "u"];
-          super-alt-down.launch = ["hyprctl" "dispatch" "movewindow" "d"];
-          super-alt-left.launch = ["hyprctl" "dispatch" "movewindow" "l"];
-          super-alt-right.launch = ["hyprctl" "dispatch" "movewindow" "r"];
+        # Move window in the same workspace with super + alt + arrow keys
+        super-alt-up.launch = ["hyprctl" "dispatch" "movewindow" "u"];
+        super-alt-down.launch = ["hyprctl" "dispatch" "movewindow" "d"];
+        super-alt-left.launch = ["hyprctl" "dispatch" "movewindow" "l"];
+        super-alt-right.launch = ["hyprctl" "dispatch" "movewindow" "r"];
 
-          super-1.launch = ["${workspace-change}" "1"];
-          super-2.launch = ["${workspace-change}" "2"];
-          super-3.launch = ["${workspace-change}" "3"];
-          super-4.launch = ["${workspace-change}" "4"];
-          super-5.launch = ["${workspace-change}" "5"];
-          super-6.launch = ["${workspace-change}" "6"];
-          super-7.launch = ["${workspace-change}" "7"];
-          super-8.launch = ["${workspace-change}" "8"];
-          super-9.launch = ["${workspace-change}" "9"];
-          super-0.launch = ["${workspace-change}" "10"];
-          super-space.launch = ["${workspace-change}" "11"];
-          super-s.launch = ["${workspace-change}" "12"];
-          super-k.launch = ["${workspace-change}" "13"];
-          super-m.launch = ["${workspace-change}" "14"];
+        super-1.launch = ["${workspace-change}" "1"];
+        super-2.launch = ["${workspace-change}" "2"];
+        super-3.launch = ["${workspace-change}" "3"];
+        super-4.launch = ["${workspace-change}" "4"];
+        super-5.launch = ["${workspace-change}" "5"];
+        super-6.launch = ["${workspace-change}" "6"];
+        super-7.launch = ["${workspace-change}" "7"];
+        super-8.launch = ["${workspace-change}" "8"];
+        super-9.launch = ["${workspace-change}" "9"];
+        super-0.launch = ["${workspace-change}" "10"];
+        super-space.launch = ["${workspace-change}" "11"];
+        super-s.launch = ["${workspace-change}" "12"];
+        super-k.launch = ["${workspace-change}" "13"];
+        super-m.launch = ["${workspace-change}" "14"];
 
-          super-shift-1.launch = ["hyprctl" "dispatch" "movetoworkspace" "1"];
-          super-shift-2.launch = ["hyprctl" "dispatch" "movetoworkspace" "2"];
-          super-shift-3.launch = ["hyprctl" "dispatch" "movetoworkspace" "3"];
-          super-shift-4.launch = ["hyprctl" "dispatch" "movetoworkspace" "4"];
-          super-shift-5.launch = ["hyprctl" "dispatch" "movetoworkspace" "5"];
-          super-shift-6.launch = ["hyprctl" "dispatch" "movetoworkspace" "6"];
-          super-shift-7.launch = ["hyprctl" "dispatch" "movetoworkspace" "7"];
-          super-shift-8.launch = ["hyprctl" "dispatch" "movetoworkspace" "8"];
-          super-shift-9.launch = ["hyprctl" "dispatch" "movetoworkspace" "9"];
-          super-shift-0.launch = ["hyprctl" "dispatch" "movetoworkspace" "10"];
-          super-shift-space.launch = ["hyprctl" "dispatch" "movetoworkspace" "11"];
-          super-shift-s.launch = ["hyprctl" "dispatch" "movetoworkspace" "12"];
-          super-shift-k.launch = ["hyprctl" "dispatch" "movetoworkspace" "13"];
-          super-shift-m.launch = ["hyprctl" "dispatch" "movetoworkspace" "14"];
+        super-shift-1.launch = ["hyprctl" "dispatch" "movetoworkspace" "1"];
+        super-shift-2.launch = ["hyprctl" "dispatch" "movetoworkspace" "2"];
+        super-shift-3.launch = ["hyprctl" "dispatch" "movetoworkspace" "3"];
+        super-shift-4.launch = ["hyprctl" "dispatch" "movetoworkspace" "4"];
+        super-shift-5.launch = ["hyprctl" "dispatch" "movetoworkspace" "5"];
+        super-shift-6.launch = ["hyprctl" "dispatch" "movetoworkspace" "6"];
+        super-shift-7.launch = ["hyprctl" "dispatch" "movetoworkspace" "7"];
+        super-shift-8.launch = ["hyprctl" "dispatch" "movetoworkspace" "8"];
+        super-shift-9.launch = ["hyprctl" "dispatch" "movetoworkspace" "9"];
+        super-shift-0.launch = ["hyprctl" "dispatch" "movetoworkspace" "10"];
+        super-shift-space.launch = ["hyprctl" "dispatch" "movetoworkspace" "11"];
+        super-shift-s.launch = ["hyprctl" "dispatch" "movetoworkspace" "12"];
+        super-shift-k.launch = ["hyprctl" "dispatch" "movetoworkspace" "13"];
+        super-shift-m.launch = ["hyprctl" "dispatch" "movetoworkspace" "14"];
 
-          super-shift-o.launch = ["hyprctl" "dispatch" "togglesplit"];
-          super-shift-v.launch = ["hyprctl" "dispatch" "togglefloating"];
-        };
-      }
-      # Laziness Mode
-      {
-        name = "laziness-mode";
-        application.only = ["imv" "mpv"];
-        remap = {
-          delete.launch = ["hyprctl" "dispatch" "killactive"];
-        };
-      }
-    ]
-    ++ (
-      if user.machine == "desktop"
-      then [
-        # Cursor Keyboard
-        {
-          name = "cursor-keyboard";
-          remap = {
-            control-shift-alt-up.launch = ["ydotool" "mousemove" "-x" "0" "-y" "-5"];
-            control-shift-alt-down.launch = ["ydotool" "mousemove" "-x" "0" "-y" "5"];
-            control-shift-alt-left.launch = ["ydotool" "mousemove" "-x" "-5" "-y" "0"];
-            control-shift-alt-right.launch = ["ydotool" "mousemove" "-x" "5" "-y" "0"];
-          };
-        }
-        # Headphones
-        {
-          name = "headphones";
-          remap = {
-            volumeup.launch = [
-              "${ # Script to prevent volume going up higher than 100
-                pkgs.writeShellScript "volume-up.sh" ''
-                  current_volume=$(${pkgs.pulseaudio}/bin/pactl list sinks | grep 'Volume: front-left' | awk '{print $5}' | sed 's/%//')
-                  if [ "$current_volume" -lt 100 ]; then
-                    ${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 +5%
-                  fi
-                ''
-              }"
-            ];
+        super-shift-o.launch = ["hyprctl" "dispatch" "togglesplit"];
+        super-shift-v.launch = ["hyprctl" "dispatch" "togglefloating"];
+      };
+    }
+    # Cursor Keyboard
+    {
+      name = "cursor-keyboard";
+      remap = {
+        control-shift-alt-up.launch = ["ydotool" "mousemove" "-x" "0" "-y" "-5"];
+        control-shift-alt-down.launch = ["ydotool" "mousemove" "-x" "0" "-y" "5"];
+        control-shift-alt-left.launch = ["ydotool" "mousemove" "-x" "-5" "-y" "0"];
+        control-shift-alt-right.launch = ["ydotool" "mousemove" "-x" "5" "-y" "0"];
+      };
+    }
+    # Headphones
+    {
+      name = "headphones";
+      remap = {
+        volumeup.launch = [
+          "${ # Script to prevent volume going up higher than 100
+            pkgs.writeShellScript "volume-up.sh" ''
+              current_volume=$(${pkgs.pulseaudio}/bin/pactl list sinks | grep 'Volume: front-left' | awk '{print $5}' | sed 's/%//')
+              if [ "$current_volume" -lt 100 ]; then
+                ${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 +5%
+              fi
+            ''
+          }"
+        ];
 
-            volumedown.launch = ["${pkgs.pulseaudio}/bin/pactl" "set-sink-volume" "0" "-5%"];
-          };
-        }
-      ]
-      else []
-    );
+        volumedown.launch = ["${pkgs.pulseaudio}/bin/pactl" "set-sink-volume" "0" "-5%"];
+      };
+    }
+    # Laziness Mode
+    {
+      name = "laziness-mode";
+      application.only = ["imv" "mpv"];
+      remap = {
+        delete.launch = ["hyprctl" "dispatch" "killactive"];
+      };
+    }
+  ];
 }
