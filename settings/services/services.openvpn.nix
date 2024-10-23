d0ab522835af05194f54dbdc7b843f7357b8bdf4
sfuentes <sfuentes@mail.com>
2024-10-23 19:07:14 -0300

    Changed: OpenVPN server (Though don't know if that makes any difference in speed)

--- settings/services/services.openvpn.nix
+++ settings/services/services.openvpn.nix
@@ -2,7 +2,7 @@
   services.openvpn.servers.protonvpn = {
     autoStart = false;
 
-    config = "config ${user.home}/Documents/Services/Internet/VPN/nl.protonvpn.udp.ovpn";
+    config = "config ${user.home}/Documents/Services/Internet/VPN/us.protonvpn.udp.ovpn";
 
     updateResolvConf = true;
   };
