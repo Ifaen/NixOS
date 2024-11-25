5b0dcefcdfd644dda2c0ef6302d7c78eb585bca6
sfuentes <sfuentes@mail.com>
2024-11-25 00:40:44 -0300

    Fix: Wezterm assigns lf keybinds to lfc properly
    - Changes on wezterm.on("update-status"), so instead of looking at the foreground process name, instead gets the tty name and using 'ps' to find if terminal is running lf (useful for lfcd / lfc)

--- software/wezterm/keybinds.nix
+++ software/wezterm/keybinds.nix
@@ -1,91 +1,64 @@
 {pkgs, ...}: let
+  # Keybinds that should always be active
+  general-keybindings = ''
+    { key='t', mods='CTRL', action=wezterm.action.SpawnTab 'CurrentPaneDomain' }, -- New tab
+    { key='w', mods='CTRL', action=wezterm.action.CloseCurrentTab { confirm = true } }, -- Close tab
+
+    -- Disable defaults
+    { key="C", mods="CTRL|SHIFT", action=wezterm.action.DisableDefaultAssignment },
+    { key="V", mods="CTRL|SHIFT", action=wezterm.action.DisableDefaultAssignment },
+    { key='t', mods='SUPER', action=wezterm.action.DisableDefaultAssignment },
+    { key='T', mods='CTRL|SHIFT', action=wezterm.action.DisableDefaultAssignment },
+    { key="V", mods="CTRL|SHIFT", action=wezterm.action.DisableDefaultAssignment },
+    { key='W', mods='CTRL|SHIFT', action=wezterm.action.DisableDefaultAssignment },
+
+    -- Disable key press
+    { key="1", mods="SUPER", action=wezterm.action.Nop },
+    { key="2", mods="SUPER", action=wezterm.action.Nop },
+    { key="3", mods="SUPER", action=wezterm.action.Nop },
+    { key="4", mods="SUPER", action=wezterm.action.Nop },
+    { key="5", mods="SUPER", action=wezterm.action.Nop },
+    { key="6", mods="SUPER", action=wezterm.action.Nop },
+    { key="7", mods="SUPER", action=wezterm.action.Nop },
+    { key="8", mods="SUPER", action=wezterm.action.Nop },
+    { key="9", mods="SUPER", action=wezterm.action.Nop },
+    { key="0", mods="SUPER", action=wezterm.action.Nop },
+  '';
+
+  # Modified keybinds
+  modified-keybindings = ''
+    { key='c', mods='CTRL', action=wezterm.action.CopyTo 'Clipboard' }, -- Copy
+    {
+      key='v', mods='CTRL', -- Paste
+      -- # HACK because PasteFrom in Wayland doesn't seem to work, obtained from: https://github.com/wez/wezterm/issues/3968
+      action=wezterm.action_callback(function(window, pane)
+        local success, stdout = wezterm.run_child_process({"${pkgs.wl-clipboard-rs}/bin/wl-paste", "--no-newline"})
+        if success then
+          pane:paste(stdout)
+        end
+      end),
+    },
+    { key='Escape', action=wezterm.action.SendString '\x03' }, -- Make Escape to cancel current process
+  '';
+
   commands = ''
     wezterm.on("update-status", function(window, pane)
-      local process_name = pane:get_foreground_process_name()
-      -- Prevent the modified keybindings to overwrite the other software keybindings
-      if process_name == "${pkgs.lf}/bin/lf" then
-        window:set_config_overrides({keys = default_keybindings})
+      local tty = pane:get_tty_name()
+
+      local _, process_name, _ = wezterm.run_child_process({
+        "${pkgs.bash}/bin/bash", "-c", "ps -t " .. tty .. " | grep '[l]f' | awk '{print $4}'"
+      })
+
+      if process_name == "lf\n" then
+        window:set_config_overrides({keys = {${general-keybindings}}})
       else
-        window:set_config_overrides({keys = modified_keybindings})
+        window:set_config_overrides({keys = {${general-keybindings}${modified-keybindings}}})
       end
     end)
-  '';
-
-  modified-keybindings = ''
-    local modified_keybindings = {
-      -- Bind control+c to copy
-      {
-        key="c",
-        mods="CTRL",
-        action=wezterm.action.CopyTo 'Clipboard',
-      },
-      -- Bind control+v to paste # HACK because PasteFrom in Wayland doesn't seem to work, obtained from: https://github.com/wez/wezterm/issues/3968
-      {
-        key="v",
-        mods="CTRL",
-        action=wezterm.action_callback(function(window, pane)
-          local success, stdout = wezterm.run_child_process({"${pkgs.wl-clipboard-rs}/bin/wl-paste", "--no-newline"})
-          if success then
-            pane:paste(stdout)
-          end
-        end),
-      },
-      -- Make Escape to cancel current process
-      {
-        key='Escape',
-        action=wezterm.action.SendString '\x03'
-      },
-      -- Disable default copy
-      {
-        key="c",
-        mods="CTRL|SHIFT",
-        action=wezterm.action.Nop,
-      },
-      -- Disable default paste
-      {
-        key="v",
-        mods="CTRL|SHIFT",
-        action=wezterm.action.Nop,
-      },
-    }
-  '';
-
-  # Restore the default keybindings while inside other TUI's
-  default-keybindings = ''
-    local default_keybindings = {
-      {
-        key="c",
-        mods="CTRL",
-        action=wezterm.action.SendKey { key = 'c', mods = 'CTRL' },
-      },
-      {
-        key="C",
-        mods="CTRL|SHIFT",
-        action=wezterm.action.SendKey { key = 'c', mods = 'CTRL|SHIFT' },
-      },
-      {
-        key="v",
-        mods="CTRL",
-        action=wezterm.action.SendKey { key = 'v', mods = 'CTRL' },
-      },
-      {
-        key="V",
-        mods="CTRL|SHIFT",
-        action=wezterm.action.SendKey { key = 'V', mods = 'CTRL|SHIFT' },
-      },
-      {
-        key='Escape',
-        action=wezterm.action.SendKey { key = 'Escape' },
-      },
-    }
   '';
 in {
   user.manage = {
     programs.wezterm.extraConfig = ''
-      ${modified-keybindings}
-
-      ${default-keybindings}
-
       ${commands}
 
       return config
