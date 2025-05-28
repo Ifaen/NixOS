82fafec9e655ae652e3193514cbc90377dd6e36e
sfuentes <dev@sfuentes.cl>
2025-05-28 18:19:37 -0300

    Added something I forgot
    Im dumb, forgot to test the correct way to use the vscode-utils.buildVscodeMarketplaceExtension
    
    Now it works.

--- software/vscode/extensions.nix
+++ software/vscode/extensions.nix
@@ -4,19 +4,12 @@
   ...
 }: {
   user-manage = {
-    home.packages = [pkgs.alejandra];
+    home.packages = [pkgs.alejandra]; # Needed for extension to work correctly
 
-    programs.vscode.profiles.default.extensions =
-      (with pkgs.vscode-extensions; [
-        jnoortheen.nix-ide # Nix language support
-        kamadorueda.alejandra # Nix formatting plugin
-        esbenp.prettier-vscode # General formatting plugin
-        pkief.material-icon-theme # Icons
-      ])
+    programs.vscode.profiles.default.extensions = let
       # NOTE: dlasagno.wal-theme is installed manually because the wal.json file is incompatible with declarative configuration
       # Extensions built from source. Used sha256=lib.fakeSha256; to get the correct sha256
-      ++ [
-        # Continue extension. Allows to run local Ollama models
+      continue =
         pkgs.vscode-utils.buildVscodeMarketplaceExtension
         {
           mktplcRef = {
@@ -29,7 +22,16 @@
           # Patch obtained from: https://github.com/continuedev/continue/issues/821
           nativeBuildInputs = [pkgs.autoPatchelfHook];
           buildInputs = [pkgs.stdenv.cc.cc.lib];
-        }
+        };
+    in
+      (with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide # Nix language support
+        kamadorueda.alejandra # Nix formatting plugin
+        esbenp.prettier-vscode # General formatting plugin
+        pkief.material-icon-theme # Icons
+      ])
+      ++ [
+        continue # Continue extension. Allows to run local Ollama models
       ];
 
     programs.vscode.profiles.default.enableExtensionUpdateCheck = false; # Whether to prevent auto update messages
