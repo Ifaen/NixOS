5a41bf1a72816e802aa8aff16b6839e6f683d8de
sfuentes <dev@sfuentes.cl>
2025-07-17 01:26:46 -0300

    Added Davinci Resolve

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -2,4 +2,8 @@
   imports = [
     ./hardware-configuration.nix # WARNING: INITIALLY REPLACE CONTENT WITH /etc/nixos/hardware-configuration.nix
   ];
+
+  config = {
+    davinciResolve.enable = true;
+  };
 }
